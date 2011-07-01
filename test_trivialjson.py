cacc17df29e8d7819d5e612466738785ba1475bc
Philipp Hagemeister <phihag@phihag.de>
2011-07-01 18:29:30 +0200

    Unicode tests

--- test_trivialjson.py
+++ test_trivialjson.py
@@ -50,6 +50,33 @@ def test_backslashes():
 	assertInvalid(r'"\\\"')
 	assert loads(r'"\\\\"') == r'\\'
 
+def test_escapes():
+	assert loads('"\\' + chr(0x22) + '"') == chr(0x22)
+	assert loads('"\\' + chr(0x5C) + '"') == chr(0x5C)
+	assert loads('"\\' + chr(0x2F) + '"') == chr(0x2F)
+	assert loads('"\\' + chr(0x62) + '"') == chr(0x08)
+	assert loads('"\\' + chr(0x66) + '"') == chr(0x0C)
+	assert loads('"\\' + chr(0x6E) + '"') == chr(0x0A)
+	assert loads('"\\' + chr(0x72) + '"') == chr(0x0D)
+	assert loads('"\\' + chr(0x74) + '"') == chr(0x09)
+
+	assertInvalid('"\\' + chr(0x21) + '"')
+	assertInvalid('"\\' + chr(0x63) + '"')
+	assertInvalid('"\\\'"')
+
+def test_unicode():
+	assert loads('"\u1234"'.encode('UTF-8')) == unichr(0x1234)
+	assert loads('"\u0000"'.encode('UTF-8')) == unichr(0x0000)
+	assert loads('"\u1000\u2000"'.encode('UTF-8')) == unichr(0x1000) + unichr(0x2000)
+
+def test_unicode_escapes_bmp():
+	assert loads('"\\u1234"'.encode('UTF-8')) == unichr(0x1234)
+	assert loads('"\\u0000"'.encode('UTF-8')) == unichr(0x0000)
+	assert loads('"\\u1000\\u2000"'.encode('UTF-8')) == unichr(0x1000) + unichr(0x2000)
+
+def test_unicode_escapes_complex():
+	assert loads('"\\uD834\\uDD1E"'.encode('UTF-8')) == unichr(0x1d11e)
+
 def test_skipspace():
 	assert loads('1 ') == 1
 	assert loads(' { } ') == {}
